7f9d94711feda312c4980cbbb2adeb85ebab3487
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2023-12-04 16:24:44 -0300

    Configurações do vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  resolve: {
+    alias: {
+      '/@/components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+};
