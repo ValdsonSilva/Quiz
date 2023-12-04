893cfc21b8242d88f670c43f203c4f3f85fcb3a2
Valdsonmacedo <Valdsonmacedo15@gmail.com>
2023-12-04 13:07:24 -0300

    Adicionando novas perguntas

--- src/data/questions.js
+++ src/data/questions.js
@@ -29,6 +29,41 @@ const data = [
     options: ["qty", "length", "items", "index"],
     answer: "length",
   },
+  {
+    question: "Qual é a diferença entre == e === em JavaScript?",
+    options: ["== compara valores e tipos, === compara apenas valores.", 
+              "== compara apenas valores, === compara valores e tipos.", 
+              "Ambos comparam apenas valores.", 
+              "Ambos comparam valores e tipos."],
+    answer: "== compara apenas valores, === compara valores e tipos.", 
+  },
+  {
+    question: "O que é hoisting em JavaScript?",
+    options:  ["É uma técnica de otimização de código",
+               "É a elevação de declarações de variáveis e funções durante a fase de compilação.",
+               "É o processo de criar closures em tempo de execução.",
+                "Não é um conceito válido em JavaScript."],
+
+    answer: "É a elevação de declarações de variáveis e funções durante a fase de compilação.",
+  },
+  {
+    question: "O que é o DOM em JavaScript?",
+    options:  ["Uma linguagem de programação.",
+               "Uma abordagem de programação assíncrona.",
+               "Um modelo de objetos para representar documentos HTML e XML.",
+                "Uma biblioteca JavaScript popular."],
+                
+    answer: "Um modelo de objetos para representar documentos HTML e XML.",
+  },
+  {
+    question: "Como você adicionaria um evento de clique a um elemento HTML usando JavaScript?",
+    options:  ["element.onclick = minhaFuncao;",
+               "element.addEventListener('click', minhaFuncao);",
+               "element.click(meuEvento);",
+               "element.attachEvent('click', minhaFuncao);"],
+                
+    answer: "element.addEventListener('click', minhaFuncao);",
+  }
 ];
 
 export default data;
